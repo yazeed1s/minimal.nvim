3e76c127adef584fdc54f61fb33b0ad4fb6b8dc6
Yazeed1s <Yazeed1s@users.noreply.github.com>
2022-08-17 19:27:13 -0400

    add key 'minimal_'

--- lua/minimal/config.lua
+++ lua/minimal/config.lua
@@ -3,6 +3,7 @@ local config
 vim = vim or { g = {}, o = {} }
 
 local function opt(key, default)
+  key = "minimal_" .. key
   if vim.g[key] == nil then
     return default
   end
@@ -14,10 +15,11 @@ end
 
 config = {
   transparent_background = opt('transparent_background', false),
-  italic_comments = opt('italic_keywords', true) and 'italic' or 'NONE',
-  italic_keywords = opt('italic_keywords', true) and 'italic' or 'NONE',
-  italic_functions = opt('italic_function', false) and 'italic' or 'NONE',
-  italic_variables = opt('italic_variables', true) and 'italic' or 'NONE',
+  italic_comments = opt('italic_comments', true) and 'italic' or 'NONE',
+  italic_keywords = opt('italic_keywords', false) and 'italic' or 'NONE',
+  italic_functions = opt('italic_functions', false) and 'italic' or 'NONE',
+  italic_booleans = opt('italic_booleans', false) and 'italic' or 'NONE',
+  italic_variables = opt('italic_variables', false) and 'italic' or 'NONE',
 }
 
 return config
